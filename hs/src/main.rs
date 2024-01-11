899aed855cfd58f25ad15e43c250acd2151c00f4
Kenton Hamaluik <kenton@hamaluik.ca>
2024-01-11 08:48:16 -0700

    added info printing to hs

--- hs/src/main.rs
+++ hs/src/main.rs
@@ -2,8 +2,10 @@ use bytes::BytesMut;
 use color_eyre::eyre::{Context, Result};
 use futures::{SinkExt, StreamExt};
 use hl7_mllp_codec::MllpCodec;
-use termcolor::StandardStream;
+use std::fmt::Display;
+use std::io::Write;
 use std::{net::SocketAddr, time::Duration};
+use termcolor::{Color, ColorSpec, StandardStream, WriteColor};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::time::timeout;
 use tokio_util::codec::Framed;
@@ -11,11 +13,56 @@ use tokio_util::codec::Framed;
 mod ack;
 mod cli;
 
+fn log<S: Display>(s: S, level: u8, stdout: &mut StandardStream) -> Result<()> {
+    let mut colour = ColorSpec::new();
+    let colour = match level {
+        1 => colour.set_fg(Some(Color::Rgb(156, 207, 216))),
+        2 => colour.set_fg(Some(Color::Rgb(246, 193, 119))),
+        3 => colour.set_fg(Some(Color::Rgb(49, 116, 143))),
+        _ => colour.set_fg(Some(Color::White)),
+    };
+
+    stdout
+        .set_color(&colour)
+        .wrap_err_with(|| "Failed to set terminal colour")?;
+    writeln!(stdout, "{}", s).wrap_err_with(|| "Failed to write to stdout/err")?;
+    stdout
+        .reset()
+        .wrap_err_with(|| "Failed to reset terminal colour")?;
+
+    Ok(())
+}
+
+macro_rules! info {
+    ($stdout:ident, $loglevel:ident, $($arg:tt)*) => {
+        if $loglevel >= 1 {
+            log(format!($($arg)*), 1, &mut $stdout).wrap_err_with(|| "Failed to log info message")?;
+        }
+    };
+}
+
+macro_rules! debug {
+    ($stdout:ident, $loglevel:ident, $($arg:tt)*) => {
+        if $loglevel >= 2 {
+            log(format!($($arg)*), 2, &mut $stdout).wrap_err_with(|| "Failed to log debug message")?;
+        }
+    };
+}
+
+macro_rules! trace {
+    ($stdout:ident, $loglevel:ident, $($arg:tt)*) => {
+        if $loglevel >= 3 {
+            log(format!($($arg)*), 3, &mut $stdout).wrap_err_with(|| "Failed to log debug message")?;
+        }
+    };
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
 
     let cli = cli::cli();
+    let loglevel = cli.verbose;
     let mut stdout = open_stdout(&cli);
 
     match cli.command {
@@ -26,9 +73,11 @@ async fn main() -> Result<()> {
             input,
         } => {
             let input = if let Some(input) = &input {
+                info!(stdout, loglevel, "Reading input from file: {:?}", input);
                 std::fs::read_to_string(input)
                     .wrap_err_with(|| format!("Failed to read input file: {:?}", input.display()))?
             } else {
+                info!(stdout, loglevel, "Reading input from stdin");
                 use std::io::Read;
                 let mut input = String::new();
                 std::io::stdin()
@@ -37,37 +86,57 @@ async fn main() -> Result<()> {
                 input
             };
             let input = strip_ansi_escapes::strip_str(input);
+            trace!(stdout, loglevel, "Read input:\n{:?}", input);
 
             let input = if cli.no_correct_newlines {
+                trace!(stdout, loglevel, "Not correcting newlines");
                 input
             } else {
+                trace!(stdout, loglevel, "Correcting newlines");
                 correct_newlines(&input)
             };
             let input = input.trim_end_matches('\r').to_string();
+            trace!(stdout, loglevel, "Corrected input:\n{:?}", input);
 
             if !no_parse {
+                debug!(stdout, loglevel, "Parsing input");
                 hl7_parser::ParsedMessage::parse(&input)
                     .wrap_err_with(|| "Failed to parse input message")?;
             }
 
+            debug!(
+                stdout,
+                loglevel, "Connecting to HL7 destination: {}", destination
+            );
             let stream = TcpStream::connect(destination)
                 .await
                 .with_context(|| "Failed to connect to HL7 destination!")?;
             let mut transport = Framed::new(stream, MllpCodec::new());
+            info!(
+                stdout,
+                loglevel, "Connected to HL7 destination: {}", destination
+            );
 
+            debug!(stdout, loglevel, "Sending message");
             transport
                 .send(BytesMut::from(input.as_bytes()))
                 .await
                 .wrap_err_with(|| "Failed to send message")?;
+            info!(stdout, loglevel, "Sent message");
 
+            debug!(stdout, loglevel, "Waiting for response");
             let message_response = timeout(Duration::from_secs_f64(wait_time), transport.next())
                 .await
                 .ok()
                 .flatten();
             if let Some(received) = message_response {
+                info!(stdout, loglevel, "Received response");
                 let received = received.wrap_err_with(|| "Failed to receive response")?;
+                trace!(stdout, loglevel, "Response bytes:\n{:?}", received);
                 let message = String::from_utf8_lossy(&received);
                 print_message(message, &mut stdout).wrap_err_with(|| "Failed to print message")?;
+            } else {
+                info!(stdout, loglevel, "No response received");
             }
         }
         cli::Command::Listen {
@@ -75,53 +144,72 @@ async fn main() -> Result<()> {
             ack_mode,
             port,
         } => {
+            debug!(stdout, loglevel, "Starting to listen on 0.0.0.0:{}", port);
             let addr = SocketAddr::from(([0, 0, 0, 0], port));
             let listener = TcpListener::bind(&addr)
                 .await
                 .wrap_err_with(|| format!("Failed to start listening on {addr}"))?;
+            info!(stdout, loglevel, "Listening on 0.0.0.0:{}", port);
 
             let mut received_messages: usize = 0;
             'accept: loop {
                 let Ok((stream, _)) = listener.accept().await else {
+                    info!(stdout, loglevel, "Failed to accept connection");
                     continue 'accept;
                 };
 
-                    let mut transport = Framed::new(stream, MllpCodec::new());
-                    'messages: while let Some(result) = transport.next().await {
-                        let Ok(message) = result else {
-                            break 'messages;
-                        };
-                        let message = String::from_utf8_lossy(&message);
-                        let message = if cli.no_correct_newlines {
-                            message.to_string()
-                        } else {
-                            correct_newlines(message.as_ref())
-                        };
+                let mut transport = Framed::new(stream, MllpCodec::new());
+                'messages: while let Some(result) = transport.next().await {
+                    trace!(stdout, loglevel, "Received message");
+                    trace!(stdout, loglevel, "Message bytes:\n{:?}", result);
+                    let Ok(message) = result else {
+                        break 'messages;
+                    };
+                    let message = String::from_utf8_lossy(&message);
+                    let message = if cli.no_correct_newlines {
+                        trace!(stdout, loglevel, "Not correcting newlines");
+                        message.to_string()
+                    } else {
+                        trace!(stdout, loglevel, "Correcting newlines");
+                        correct_newlines(message.as_ref())
+                    };
 
-                        let ack = match ack_mode {
-                            cli::AckMode::Success => {
-                                Some(ack::generate_ack(&message, cli::AckMode::Success)
-                                    .wrap_err_with(|| "Failed to generate ACK")?)
-                            }
-                            cli::AckMode::Error => {
-                                Some(ack::generate_ack(&message, cli::AckMode::Error)
-                                    .wrap_err_with(|| "Failed to generate ACK")?)
-                            }
-                            cli::AckMode::Ignore => {
-                                None
-                            }
-                        };
-                        if let Some((ack, _parsed_message)) = ack {
-                            transport
-                                .send(BytesMut::from(ack.as_bytes()))
-                                .await
-                                .wrap_err_with(|| "Failed to send ACK")?;
+                    let ack = match ack_mode {
+                        cli::AckMode::Success => {
+                            debug!(stdout, loglevel, "Generating success ACK");
+                            Some(
+                                ack::generate_ack(&message, cli::AckMode::Success)
+                                    .wrap_err_with(|| "Failed to generate ACK")?,
+                            )
                         }
-                        print_message(message, &mut stdout).wrap_err_with(|| "Failed to print message")?;
+                        cli::AckMode::Error => {
+                            debug!(stdout, loglevel, "Generating error ACK");
+                            Some(
+                                ack::generate_ack(&message, cli::AckMode::Error)
+                                    .wrap_err_with(|| "Failed to generate ACK")?,
+                            )
+                        }
+                        cli::AckMode::Ignore => {
+                            debug!(stdout, loglevel, "Not generating ACK");
+                            None
+                        }
+                    };
+                    if let Some((ack, _parsed_message)) = ack {
+                        info!(stdout, loglevel, "Sending ACK");
+                        debug!(stdout, loglevel, "ACK:\n{}", ack);
+                        transport
+                            .send(BytesMut::from(ack.as_bytes()))
+                            .await
+                            .wrap_err_with(|| "Failed to send ACK")?;
+                    }
+                    print_message(message, &mut stdout)
+                        .wrap_err_with(|| "Failed to print message")?;
 
                     received_messages += 1;
+                    trace!(stdout, loglevel, "Received {} messages so far", received_messages);
                     if let Some(message_count) = message_count {
                         if received_messages >= message_count {
+                            info!(stdout, loglevel, "Received {} messages, quitting", received_messages);
                             break 'accept;
                         }
                     }
@@ -142,14 +230,13 @@ fn open_stdout(cli: &cli::Cli) -> StandardStream {
     StandardStream::stderr(colour)
 }
 
+fn correct_newlines(message: &str) -> String {
+    message.replace("\r\n", "\r").replace('\n', "\r")
+}
+
 fn print_message<S: ToString>(message: S, stdout: &mut StandardStream) -> Result<()> {
-    use std::io::Write;
     let message = message.to_string().replace('\r', "\n");
     writeln!(stdout, "{}", message).wrap_err_with(|| "Failed to write to stdout")?;
     writeln!(stdout).wrap_err_with(|| "Failed to write to stdout")?;
     Ok(())
-}
-
-fn correct_newlines(message: &str) -> String {
-    message.replace("\r\n", "\r").replace('\n', "\r")
 }
